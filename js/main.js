54b3a5ae6ccec150b0797ed1156d0964440c4c58
AnnaKubiak <a.kubiak@wearevirtua.com>
2020-07-08 10:56:36 +0200

    initial commit

---
+++ js/main.js
@@ -0,0 +1,14 @@
+// jQuery(document).ready(function () {
+//   jQuery("#my_nanogallery2").nanogallery2({
+//     items: [
+//       { src: "./../img/Originals/1.JPG", srct: "./../img/1.JPG" },
+//       { src: "berlin2.jpg", srct: "berlin2t.jpg" },
+//       { src: "berlin3.jpg", srct: "berlin3t.jpg" },
+//     ],
+//     thumbnailWidth: "auto",
+//     thumbnailHeight: 100,
+//     itemsBaseURL: "https://photos.google.com/?tab=kq&pageId=none",
+
+//     locationHash: false,
+//   });
+// });
